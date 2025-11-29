e8a22d41f4985fc84dd74bfcc089e7cd0458a0b6
npremont <ni.premont@gmail.com>
2025-11-29 20:24:28 +0100

    new: SEO standards

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.hordeagence.com',
   output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
-
+  integrations: [sitemap()],
   adapter: node({
     mode: 'standalone'
   })
